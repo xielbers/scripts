c94e3afc1c5f07012978931265f999d57aaa4951
xielbers <88589081+xielbers@users.noreply.github.com>
2025-01-23 22:07:50 -0300

    Create blade-ball.lua

---
+++ blade-ball.lua
@@ -0,0 +1,53 @@
+local RunService = game:GetService("RunService") or game:FindFirstDescendant("RunService")
+local Players = game:GetService("Players") or game:FindFirstDescendant("Players")
+local VirtualInputManager = game:GetService("VirtualInputManager") or game:FindFirstDescendant("VirtualInputManager")
+
+local Player = Players.LocalPlayer
+
+local Cooldown = tick()
+local IsParried = false
+local Connection = nil
+
+local function GetBall()
+  for _, Ball in ipairs(workspace.Balls:GetChildren()) do
+    if Ball:GetAttribute("realBall") then
+      return Ball
+    end
+  end
+end
+
+local function ResetConnection()
+    if Connection then
+        Connection:Disconnect()
+        Connection = nil
+    end
+end
+
+workspace.Balls.ChildAdded:Connect(function()
+    local Ball = GetBall()
+    if not Ball then return end
+    ResetConnection()
+    Connection = Ball:GetAttributeChangedSignal("target"):Connect(function()
+        Parried = false
+    end)
+end)
+
+RunService.PreSimulation:Connect(function()
+    local Ball, HRP = GetBall(), Player.Character.HumanoidRootPart
+    if not Ball or not HRP then
+      return
+    end
+    
+    local Speed = Ball.zoomies.VectorVelocity.Magnitude
+    local Distance = (HRP.Position - Ball.Position).Magnitude
+    
+    if Ball:GetAttribute("target") == Player.Name and not Parried and Distance / Speed <= 0.55 then
+      VirtualInputManager:SendMouseButtonEvent(0, 0, 0, true, game, 0)
+      Parried = true
+      Cooldown = tick()
+      
+      if (tick() - Cooldown) >= 1 then
+        Partied = false
+      end
+    end
+end)
