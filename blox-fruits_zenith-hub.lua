b9cafd3802180f819301ec53e5c41f95c25f1c22
xielbers <88589081+xielbers@users.noreply.github.com>
2025-02-15 17:54:06 -0300

    Create blox-fruits_zenith-hub.lua

---
+++ blox-fruits_zenith-hub.lua
@@ -0,0 +1,6 @@
+print(game.PlaceId)
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SaltyHB/Poggers/refs/heads/main/Main"))()
+else
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SaltyHB/Poggers/refs/heads/main/niger"))()
+end
