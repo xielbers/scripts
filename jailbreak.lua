33bd67a977e8097f189e13c1304259e30667986d
xielbers <88589081+xielbers@users.noreply.github.com>
2025-03-01 19:05:43 -0300

    Update jailbreak.lua

--- jailbreak.lua
+++ jailbreak.lua
@@ -1,7 +1,1 @@
---[[
-Join discord (dsc.gg/sensation) for updates and support
-
-When running this script you automatically agree to our Terms of Service, stated in our discord.
-]]
-
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/730854e5b6499ee91deb1080e8e12ae3.lua"))()
+loadstring(game:HttpGet('http://scripts.projectauto.xyz/AutoRobV4'))()
