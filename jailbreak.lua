835275b5d5af8d3a637697be6b10e9473dbdad36
xielbers <88589081+xielbers@users.noreply.github.com>
2025-03-17 20:49:55 -0300

    Update jailbreak.lua

--- jailbreak.lua
+++ jailbreak.lua
@@ -1,3 +1,1 @@
-getgenv().LDKey = "LD47658597614629"
-
-loadstring(game:HttpGet([[https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/main.lua]]))()
+loadstring(game:HttpGet('http://scripts.projectauto.xyz/AutoRobV5'))()
