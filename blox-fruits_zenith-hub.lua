cbfcc85b4f894343d7db63b3bcfac0172fd807a8
xielbers <88589081+xielbers@users.noreply.github.com>
2025-03-26 11:45:04 -0300

    Update blox-fruits_zenith-hub.lua

--- blox-fruits_zenith-hub.lua
+++ blox-fruits_zenith-hub.lua
@@ -1,6 +1,1 @@
-print(game.PlaceId)
-if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SaltyHB/Poggers/refs/heads/main/Main"))()
-else
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SaltyHB/Poggers/refs/heads/main/niger"))()
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Efe0626/ZenithHub/refs/heads/main/Loader"))()
