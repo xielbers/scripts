f1241a2655e08e63d519d5c6f9b1719d9f079b69
xielbers <88589081+xielbers@users.noreply.github.com>
2025-03-27 13:08:55 -0300

    Update blox-fruits_redz-hub.lua

--- blox-fruits_redz-hub.lua
+++ blox-fruits_redz-hub.lua
@@ -3,4 +3,4 @@ local Settings = {
   Translator = true; -- true/false
 }
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))(Settings)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/newredz/BloxFruits/refs/heads/main/Source.luau"))(Settings)
