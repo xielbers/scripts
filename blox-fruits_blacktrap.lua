7c1f7dbd876ee7f6a0c1996855bddd13f7fe7a54
xielbers <88589081+xielbers@users.noreply.github.com>
2025-07-27 20:41:41 -0300

    Actualizar blox-fruits_blacktrap.lua

--- blox-fruits_blacktrap.lua
+++ blox-fruits_blacktrap.lua
@@ -1,4 +1,6 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/copy/ip.lua'))()
+_G.script_executed = "blox-fruits_blacktrap"
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/main/copy/ip.lua'))()
 
 print(game.PlaceId)
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then loadstring(game:HttpGetAsync("https://lua-library.btteam.net/script-auth.txt"))()
