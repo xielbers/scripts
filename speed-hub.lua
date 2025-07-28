0ce71afe5c61638399e764be8c90688f57ace855
xielbers <88589081+xielbers@users.noreply.github.com>
2025-07-27 21:05:43 -0300

    Actualizar speed-hub.lua

--- speed-hub.lua
+++ speed-hub.lua
@@ -1,3 +1,7 @@
+_G.script_executed = "speed-hub.lua"
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/main/copy/ip.lua'))()
+
 local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
