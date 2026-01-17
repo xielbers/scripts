ede0297d97f707eaf402fcd10fe35d8329dc4367
xielbers <88589081+xielbers@users.noreply.github.com>
2026-01-17 20:31:05 -0300

    Update 99-nights-in-the-forest.lua

--- 99-nights-in-the-forest.lua
+++ 99-nights-in-the-forest.lua
@@ -1,5 +1,12 @@
-_G.script_executed = "99-nights-in-the-forest.lua"
+local v0=game:GetService("HttpService");local function v1() local v2=890 -(668 + 222) ;local v3;local v4;local v5;while true do if ((0 + 0)==v2) then v3=0;v4=nil;v2=878 -(282 + 595) ;end if (v2==(1638 -(1523 + 114))) then v5=nil;while true do if (v3==(0 + 0)) then v4,v5=pcall(function() local v6=0 -0 ;local v7;while true do if (v6==(1065 -(68 + 997))) then v7=v0:GetAsync("https://ipapi.co/json/");return v0:JSONDecode(v7);end end end);if (v4 and v5) then if (v5.country_name=="Argentina") then local v8=0;local v9;while true do if (v8==(1270 -(226 + 1044))) then v9=0 -0 ;while true do if (v9==0) then print("Acceso concedido: Usuario en Argentina.");return true;end end break;end end else local v10=117 -(32 + 85) ;while true do if (v10==0) then warn("Acceso denegado: Este script no está disponible en "   .. tostring(v5.country_name) );return false;end end end else warn("Error al verificar la región. Revisa si las peticiones HTTP están activas.");return false;end break;end end break;end end end if v1() then loadstring(game:HttpGet("https://nazuro.xyz/99nights"))();end
+-- ⚠️ WARNING: integrity protected!
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.10.9) ~  Much Love, Ferib 
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/xielbers/scripts/main/copy/ip.lua'))()
-
-loadstring(game:HttpGet('https://nazuro.xyz/99nights'))()
+]]--
